c6f2afac9a04f89340b5614330f9e4b842033543
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-04-27 15:16:05 +0530

    update home page

--- index.php
+++ index.php
@@ -92,6 +92,154 @@
     <!-- End Text Banner -->
 
 
+    <!-- Start Most Popular Course -->
+    <div class="container mt-5">
+        <h1 class="text-center">Popular Course</h1>
+        <!-- Start Most Popular Course 1st Card Deck -->
+        <div class="card-deck mt-4">
+            <a href="#" class="btn" style="text-align:left;padding:0px;margin:0px;">
+                <div class="card">
+                    <img src="image/courseimg/guitar.jpg" alt="Guitar" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Learn Guitar Easy Way
+                        </h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quae corporis illo a quibusdam delectus ad consequatur quasi fugit, commodi incidunt ducimus, ut quas laudantium quod quis enim similique voluptas.</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">
+                            <span class="font-weight-bolder">
+                                Price: &#8377 200
+                            </span>
+                            <small><del>&#8377 2000</del></small>
+                        </p>
+                        <a href="#" class="btn btn-primary text-white font-weight-boalder float-right m-2">Enroll</a>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- End Most Popular Course 1st Card Deck -->
+        <!-- Start Most Popular Course 2nd Card Deck -->
+        <div class="card-deck mt-4">
+            <a href="#" class="btn" style="text-align:left;padding:0px;margin:0px;">
+                <div class="card">
+                    <img src="image/courseimg/python.png" alt="Python" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Learn Python
+                        </h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quae corporis illo a quibusdam delectus ad consequatur quasi fugit, commodi incidunt ducimus, ut quas laudantium quod quis enim similique voluptas.</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">
+                            <span class="font-weight-bolder">
+                                Price: &#8377 300
+                            </span>
+                            <small><del>&#8377 3000</del></small>
+                        </p>
+                        <a href="#" class="btn btn-primary text-white font-weight-boalder float-right m-2">Enroll</a>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- End Most Popular Course 2nd Card Deck -->
+        <div class="text-center m-2">
+            <a href="#" class="btn btn-danger btn-sm">View All Courses</a>
+        </div>
+    </div>
+
+    <!-- End Most Popular Course -->
+
+    <!-- Start Contact Us -->
+    <div class="container mt-4" id="Contact">
+        <h2 class="text-center mb-4">Contact Us</h2>
+        <div class="row">
+            <div class="col-md-8">
+                <form action="" method="post">
+                    <input type="text" name="name" placeholder="Name" class="form-control"><br>
+                    <input type="text" name="subject" placeholder="Subject" class="form-control"><br>
+                    <input type="text" name="email" placeholder="Email" class="form-control"><br>
+                    <textarea name="message" class="form-control" placeholder="How Can We Help You?" style="height:150px;" id=""></textarea><br>
+                    <input type="submit" value="Semd" name="submit" class="btn btn-primary"><br><br>
+                </form>
+            </div>
+            <div class="col-md-4 stripe text-white text-center">
+                <h4>MySchool</h4>
+                <p>MySchool, Near Khardah Police Station, Kolkata <br>
+                    West Bengal <br>
+                    phone-+9100000000 <br>
+                </p>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- End Contact Us -->
+
+    <!-- Start Social Follow -->
+
+    <div class="container-fluid bg-danger">
+        <div class="row text-white text-center p-1">
+            <div class="col-sm">
+                <a href="#" class="text-white social-hover"><i class="fab fa-facebook-f"></i>Facebook</a>
+            </div>
+
+
+            <div class="col-sm">
+                <a href="#" class="text-white social-hover"><i class="fab fa-twitter"></i>Twitter</a>
+            </div>
+
+            <div class="col-sm">
+                <a href="#" class="text-white social-hover"><i class="fab fa-whatsapp"></i>WhatsApp</a>
+            </div>
+            <div class="col-sm">
+                <a href="#" class="text-white social-hover"><i class="fab fa-instagram"></i>Instagram</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- start about us section -->
+
+    <div class="container-fluid p-4" style="background-color:#E9ECEF">
+        <div class="container-fluid p-4" style="background-color:#E9ECEF">
+            <div class="row text-center">
+                <div class="col-sm">
+                    <h5>About Us</h5>
+                    <p>
+                        MySchool provides universal access to the world's best education, partnering
+                        with top universities and organizations to offer courses online.
+                    </p>
+                </div>
+                <div class="col-sm">
+                    <h5>Category</h5>
+                    <a href="#" class="text-dark">Web Development</a><br>
+                    <a href="#" class="text-dark">Web Designing</a><br>
+                    <a href="#" class="text-dark">Android App Dev</a><br>
+                    <a href="#" class="text-dark">iOS Development</a><br>
+                    <a href="#" class="text-dark">Data Analysis</a><br>
+                    <a href="#" class="text-dark">Data Analysis</a><br>
+                </div>
+                <div class="col-sm">
+                    <h5>Contact Us</h5>
+                    <p>MySchool Private Limited <br> Near Police Station, <br> kolkata-700001</p>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <!-- start footer -->
+    <footer class="container-fluid bg-dark text-center p-2">
+        <small class="text-white">
+            Copyright &copy; 2026 || Made With ❤️ by Sayan Sarkar || Admin Login
+        </small>
+    </footer>
+
+
+
+
 
 
 
